7060bc10a872aec75203c28c6c6858ecba206d8d
Eiad <e.darwish@naits.gov.sy>
2025-11-08 20:34:17 +0300

    fix migration

--- database/migrations/2025_10_22_083226_create_categories_table.php
+++ database/migrations/2025_10_22_083226_create_categories_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('image_url');
             $table->string('name');
             $table->integer('order')->nullable();
             $table->boolean('is_active')->default(false);
