5ee3b1c5a45f1bf258237561855b2b9f2ea7667d
Eiaddar <e.darwish@aldrtechnology.com>
2025-11-30 13:14:50 +0300

    profile

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->spa()
+            ->profile()
             ->id('admin')
             ->path('webmaster')
             ->login()
